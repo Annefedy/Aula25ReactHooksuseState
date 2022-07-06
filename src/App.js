0e0b56597fbc1629289eaace1362491ff27577b4
Annefedy <103973102+Annefedy@users.noreply.github.com>
2022-07-06 01:46:59 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <div>
+      <hr />
+      Counter component (useState example):
+      <Counter initialCount={1} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Counter.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+export default function Counter({ initialCount }) {
+  const [count, setCount] = useState(initialCount);
+
+  return (
+    <div>
+      Count: {count}
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+}
